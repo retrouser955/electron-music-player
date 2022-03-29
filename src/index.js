b647ce915422b5cbe54b8a0a2ff71093844315f3
retrouser955 <htoomyataung382@gmail.com>
2022-03-29 10:54:38 +0630

    🚀 first commit

---
+++ src/index.js
@@ -0,0 +1,110 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const RPC = require('discord-rpc')
+RPC.register('955818281078497320');
+const client = new RPC.Client({ transport: 'ipc' });
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+const fs = require('fs')
+let songName;
+let files = []
+const songFiles = fs.readdirSync(`${__dirname}/songs`).filter(file => file.endsWith('.mp3'))
+let amountOfFiles = fs.readdirSync(`${__dirname}/songs`).length
+amountOfFiles = Number(amountOfFiles)
+function textReplace(haystack, needle, replacement) {
+    needle = needle.replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, "\\$1").replace(/\x08/g, "\\x08");
+    return haystack.replace(new RegExp(needle, 'g'), replacement);
+}
+for(const songs of songFiles) {
+    console.log(songs)
+    let songDisplayName;
+    songDisplayName = textReplace(songs, '.mp3', '')
+    songDisplayName = textReplace(songDisplayName, '_', ' ')
+    files.push({
+        name: `${__dirname}/songs/${songs}`,
+        thumbnail: `${__dirname}/image/record.png`,
+        displayName: `${songDisplayName}`
+    })
+    console.log(songDisplayName)
+}
+const Store = require('electron-store')
+const store = new Store()
+store.set('files', files)
+const events = require('events');
+const eventEmitter = new events.EventEmitter()
+let mainWindow;
+const createWindow = async () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 610,
+    height: 392,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: `${__dirname}/images/retro-player.png`
+  });
+  store.set('songName', 'Music Idle')
+  store.set('songState', ['Paused', 'pause'])
+  mainWindow.setResizable(false)
+  
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+async function setActivity() {
+  if (!client || !mainWindow) {
+    return;
+  }
+  songName = store.get('songName')
+  let songState = store.get('songState')
+  mainWindow.webContents
+  client.setActivity({
+      details: songName,
+      state: 'Listening',
+      largeImageKey: 'retro_player_530x530',
+      largeImageText: 'Listening to music',
+      smallImageKey: songState[1],
+      smallImageText: songState[0]
+  });
+}
+client.on('ready', () => {
+  setActivity();
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+let clientId = '955818281078497320'
+store.set('error', false)
+client.login({ clientId }).catch(e => {
+  store.set('error', true)
+});
---
+++ src/render.js
@@ -0,0 +1,173 @@
+const audio = document.getElementById('audio');
+const Store = require('electron-store')
+const ms = require('ms')
+const {shell} = require('electron')
+const icon = document.getElementById('fileOpen')
+const url = document.getElementById('url')
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const downloadBtn = document.getElementById('download-btn')
+var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+var YD = new YoutubeMp3Downloader({
+    "ffmpegPath": `${ffmpegPath}`,
+    "outputPath": `${__dirname}/songs`,
+    "youtubeVideoQuality": "highestaudio",
+    "queueParallelism": 2,
+    "progressTimeout": 2000,
+    "allowWebm": false
+});
+async function wait(number) {
+    await delay(Number(number))
+}
+const play = document.getElementById('play')
+const skip = document.getElementById('skip')
+const back = document.getElementById('back')
+const progress = document.getElementById('progress-bar');
+const progressContainer = document.getElementById('progress-container');
+const events = require('events')
+const eventEmitter = new events.EventEmitter()
+const songTitle = document.getElementById('music-title')
+const volume = document.getElementById('volume')
+play.classList.remove('play')
+const store = new Store()
+const fs = require('fs')
+const  image_finder  =  require("image-search-engine")
+let files = store.get('files')
+let error = store.get('error')
+let i = 0
+function playSong() {
+    play.classList.add('play');
+    songTitle.innerHTML = files[i].displayName
+    play.src = "./images/pause.png"
+    audio.play()
+}
+function loadSong(song) {
+    audio.src = song
+    playSong()
+}
+function nextSong() {
+    i++
+    if(i > files.length - 1) {
+        i = 0
+    }
+    loadSong(files[i].name)
+    store.set('songName', files[i].displayName)
+}
+function backSong() {
+    i--
+    if(i < 0) {
+        i = files.length - 1
+    }
+    loadSong(files[i].name)
+    store.set('songName', files[i].displayName)
+}
+audio.addEventListener('ended', () => {
+    nextSong()
+})
+function musicPlay() {
+    const isPlaying = play.classList.contains('play');
+    if(isPlaying) {
+        playSong()
+    } else {
+        return
+    }
+}
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+function pauseSong() {
+    play.src = "./images/play.png"
+    play.classList.remove('play');
+    audio.pause();
+}
+play.addEventListener('click', () => {
+    const isPlaying = play.classList.contains('play')
+    const isFirst = play.classList.contains('firstSong')
+    if(!isFirst) {
+	    loadSong(files[i].name)
+	    play.classList.add('firstSong')
+        store.set('songName', files[i].displayName)
+        store.set('songState', ['Playing', 'play'])
+    }
+    if(isPlaying) {
+        pauseSong()
+        store.set('songState', ['Paused', 'pause'])
+    } else {
+        playSong()
+        store.set('songState', ['Playing', 'play'])
+    }
+})
+back.addEventListener('click', backSong)
+skip.addEventListener('click', nextSong)
+audio.addEventListener('timeupdate', (time) => {
+    const { duration, currentTime } = time.target
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`
+})
+volume.addEventListener('change', (e) => {
+    audio.volume = e.currentTarget.value / 100;
+})
+progressContainer.addEventListener('click', setProgress)
+var toastLiveExample = document.getElementById('liveToast')
+async function wait(number) {
+    await delay(Number(number))
+}
+async function download(id) {
+    YD.download(id);
+}
+const ytDownloadError = document.getElementById('liveToastDownload')
+var toastError = new bootstrap.Toast(ytDownloadError)
+function textReplace(haystack, needle, replacement) {
+    needle = needle.replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, "\\$1").replace(/\x08/g, "\\x08");
+    return haystack.replace(new RegExp(needle, 'g'), replacement);
+}
+downloadBtn.addEventListener('click', async () => {
+    let id;
+    if(!String(url.value).toLowerCase().includes('youtube.com')) return toastError.show()
+    id = textReplace(url.value, 'youtube.com', '')
+    id = textReplace(id, '/', '')
+    id = textReplace(id, 'https', '')
+    id = textReplace(id, ':', '')
+    id = textReplace(id, 'watch?v=', '')
+    id = textReplace(id, 'www.', '')
+    var ytProgress = document.getElementById('liveToastDownloadProgress')
+    var ytProgressToast = new bootstrap.Toast(ytProgress)
+    ytProgressToast.show()
+    download(id)
+    
+    YD.on("finished", function(err, data) {
+        files = []
+        let newFiles = fs.readdirSync(`${__dirname}/songs`).filter(file => file.endsWith('.mp3'))
+        for(const songs of newFiles) {
+            let songDisplayName;
+            songDisplayName = textReplace(songs, '.mp3', '')
+            songDisplayName = textReplace(songDisplayName, '_', ' ')
+            files.push({
+                name: `${__dirname}/songs/${songs}`,
+                thumbnail: `${__dirname}/image/record.png`,
+                displayName: `${songDisplayName}`
+            })
+        }
+        i = i++
+        ytProgressToast.hide()
+        wait(5000)
+        var downloadedSongToast = document.getElementById('liveToastDownloadSuccess')
+        var downloadedToast = new bootstrap.Toast(downloadedSongToast)
+        downloadedToast.show()
+    });
+})
+var toast = new bootstrap.Toast(toastLiveExample)
+const openFile = () => {
+    shell.openPath(`${__dirname}/songs`)
+}
+icon.addEventListener('click', openFile)
+toast.hide()
+wait(4000)
+
+
+if(error === true) {
+    toast.show()
+}
